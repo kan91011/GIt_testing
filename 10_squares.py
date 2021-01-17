e2e420b72478458c0ffca1a33fa8eec71b23d4e1
kan91011 <kan91011@gmail.com>
2021-01-16 18:27:16 -0800

    1213123123123123

---
+++ 10_squares.py
@@ -0,0 +1,29 @@
+#Eight_squares
+#date 20/12/16
+#versions for loops and while loops
+
+#
+import turtle
+t = turtle.Pen()
+
+#1.
+num_sides = 4
+side_length = 200
+
+#2. draw 8 squares 
+for i in range(8):
+     #1 draw a sqware 
+     for i in range(4):
+          #turn the turtle 
+          t.lt(90)
+          t.fd(200)
+     t.lt(45)
+
+
+     
+ lrlgknjevrvernv
+ velkrnjvekjnvejrv
+ envlknlkenjrv
+ evrnlkejnvlkevnre
+ kwlnvlrnjnrwj
+
